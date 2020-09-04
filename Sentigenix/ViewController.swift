d55fd56b91e37b9139854132ac6d17205b073f09
“gokulnair2001” <gokulnair.2001@gmail.com>
2020-09-04 09:06:36 +0530

    Initial Commit

---
+++ Sentigenix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sentigenix
+//
+//  Created by Gokul Nair on 04/09/20.
+//  Copyright © 2020 Gokul Nair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
